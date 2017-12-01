f55b08bc56f9ee8e6377b2a61ab3eca873faa132
thisismrmoo <whmunro@outlook.com>
2017-12-01 13:03:23 +1100

    Add function descriptions.

--- multiClock.1s.sh
+++ multiClock.1s.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+#Establishes current system Location. Prints without "_" and Country. e.g. Melbourne
 currentZone=$(sudo systemsetup -gettimezone | sed -e 's/Time Zone://g' -e 's/.*[/]//' -e 's/_/ /g')
-currentDate=$(date +'%a %d %b %H:%M %p')
+#Establishes current system Time and Date. e.g. Fri 01 Dec 12:20 AM,
+currentDate=$(date +'%a %d %b %H:%M %p'),
+
+#List of TimeZones to display in BitBar Dropdown.
 timeZones="America/Los_Angeles America/Vancouver 	America/Winnipeg America/New_York Europe/London Europe/Paris Asia/Kolkata Asia/Bangkok Asia/Kuala_Lumpur Australia/Brisbane Australia/Melbourne Pacific/Auckland"
 
-echo $currentDate',' $currentZone
+#Displays Curent Date, Time, and Locataion in Menubar. e.g. Fri 01 Dec 12:20 AM, Melbourne
+echo $currentDate $currentZone
+
+#Creates Dropdown menu in BitBar.
 echo '---'
+
+#Attributes for Dropdown Menu per each item in "timeZones".
 for zone in $timeZones;
 do
+  #Establishes time of current location to compare to all timeZones, giving a time difference reading.
   currentD=$(date +%j:%H:%M)
   IFS=":"
   set -- $currentD
@@ -19,6 +29,7 @@ do
   currentTotalmins=$((currentDy+currentHr+currentMin))
   currentTotalHours=$(($currentTotalmins/60))
 
+  #Establishes time of all timeZones to compare to current location time, giving a time difference reading.
   zoneD=$(TZ=$zone date +%j:%H:%M)
   IFS=":"
   set -- $zoneD
@@ -30,23 +41,24 @@ do
   zoneTotalmins=$((zoneDy+zoneHr+zoneMin))
   zoneTotalHours=$(($zoneTotalmins/60))
 
+  #Establishes time difference of current location and "timeZones" in hours.
   zoneDifference=$(expr $zoneTotalHours - $currentTotalHours)
+  #Establishes time difference of current location and "timeZones" in minutes.
   zoneDifferenceMin=$(expr $zoneTotalmins - $currentTotalmins)
+  #Establishes time difference of current location and "timeZones" in minutes with coma for display. e.g -3:30
   zoneDifferenceMinComa=$(echo $zoneDifferenceMin | sed -e 's/.\{2\}$/:&/g')
 
+  #Establishes "timeZones" Time and Date. e.g. Thu 30 Nov 17:20 PM,
   dateTime=$(TZ=$zone date +'%a %d %b %H:%M %p'),
-  #location=$(echo $zone | sed -e 's/.*[/]//' -e 's/_/ /g')
-  #differenceHours=$(echo $zoneDiffhours | sed -e 's/$/HRS/')
-  #differenceMinutes=$zoneDifferenceMinComa
 
-
+  #Establishes whether to display Time Difference in hours or minutes.
   zoneDiffhours=$(echo $zoneDifference | sed -E 's/^([[:digit:]][^ ,]*).*/\+\1/')
       if [ $(( $zoneDifferenceMin % 60 )) -eq 0 ];
       then
-        #echo $dateTime $location $differenceHours
+        #Displays Date, Time, Locataion and Time Difference in hours in Dropdown. e.g. Thu 30 Nov 17:20 PM, Los Angeles -19HRS
         echo "$(TZ=$zone date +'%a %d %b %H:%M %p'), $(echo $zone | sed -e 's/.*[/]//' -e 's/_/ /g') $(echo $zoneDiffhours | sed -e 's/$/HRS/' -e 's/+0HRS//g')"
       else
-        #echo $dateTime $location $differenceMinutes
+        #Displays Date, Time, Locataion and Time Difference in minutes in Dropdown. e.g. Fre 01 Dec 07:20 AM, Kolkata -3:30
         echo "$(TZ=$zone date +'%a %d %b %H:%M %p'), $(echo $zone | sed -e 's/.*[/]//' -e 's/_/ /g') $zoneDifferenceMinComa"
       fi
 done
